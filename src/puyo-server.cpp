90a0dedb817e688bf25492be23b9ba316dc73e64
Tom Klaver <tom@tomklaver.com>
2016-01-27 13:10:53 +0100

    Add curses

--- src/puyo-server.cpp
+++ src/puyo-server.cpp
@@ -3,7 +3,9 @@
 #include <memory>
 #include <utility>
 #include <boost/asio.hpp>
-
+extern "C" {
+    #include <curses.h>
+}
 using boost::asio::ip::tcp;
 
 
@@ -101,6 +103,27 @@ private:
 
 int main(int argc, char* argv[])
 {
+    WINDOW * mainwin;
+ if ( (mainwin = initscr()) == NULL ) {
+        fprintf(stderr, "Error initialising ncurses.\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    /*  Display "Hello, world!" in the centre of the
+        screen, call refresh() to show our changes, and
+        sleep() for a few seconds to get the full screen effect  */
+
+    mvaddstr(13, 33, "Hello, world!");
+    refresh();
+    sleep(3);
+
+
+    /*  Clean up after ourselves  */
+
+    delwin(mainwin);
+    endwin();
+    refresh();
   try
   {
     if (argc != 2)
