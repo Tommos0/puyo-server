1b55d9291d9724caccac0fa43177228ba4e5fec3
Tom Klaver <tom@tomklaver.com>
2016-02-09 00:49:17 +0100

    added changes

--- src/network.h
+++ src/network.h
@@ -1,6 +1,7 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+using boost::asio::ip::tcp;
 class Connection {
     public:
         enum packet_type { 
--- src/puyo-server.cpp
+++ src/puyo-server.cpp
@@ -3,108 +3,13 @@
 #include <memory>
 #include <utility>
 #include <boost/asio.hpp>
+#include "network.h"
+#include "game.h"
+
 #ifdef CURSES
 extern "C" {
     #include <curses.h>
 }
-#endif
-
-using boost::asio::ip::tcp;
-
-
-
-class command {
-    public:
-            command() { }
-};
-
-class game {
-    public:
-       game () { };
-       std::vector<command> commandBuffer;
-};
-
-class session
-  : public std::enable_shared_from_this<session>
-{
-public:
-  session(tcp::socket socket, game g) :  game_(g), socket_(std::move(socket)) {
-    }
-  session(tcp::socket socket) :  socket_(std::move(socket))  { }
-  game game_;
-
-  void start()
-  {
-    do_read();
-  }
-
-private:
-  void do_read()
-  {
-    auto self(shared_from_this());
-    socket_.async_read_some(boost::asio::buffer(data_, max_length),
-        [this, self](boost::system::error_code ec, std::size_t length)
-        {
-          if (!ec)
-          {
-            do_write(length);
-
-            command c;
-//            self->game_.commandBuffer.push_back(c);
-
-          }
-        });
-  }
-
-  void do_write(std::size_t length)
-  {
-    auto self(shared_from_this());
-    boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
-        [this, self](boost::system::error_code ec, std::size_t /*length*/)
-        {
-          if (!ec)
-          {
-            do_read();
-          }
-        });
-  }
-
-  tcp::socket socket_;
-  enum { max_length = 1024 };
-  char data_[max_length];
-};
-
-
-class server
-{
-public:
-  server(boost::asio::io_service& io_service, short port)
-    : acceptor_(io_service, tcp::endpoint(tcp::v4(), port)),
-      socket_(io_service)
-  {
-    do_accept();
-  }
-
-private:
-  void do_accept()
-  {
-    acceptor_.async_accept(socket_,
-        [this](boost::system::error_code ec)
-        {
-          if (!ec)
-          {
-            std::make_shared<session>(std::move(socket_))->start();
-          }
-
-          do_accept();
-        });
-  }
-
-  tcp::acceptor acceptor_;
-  tcp::socket socket_;
-};
-
-#ifdef CURSES
 void curses_print() {
 
  WINDOW * mainwin;
@@ -112,7 +17,6 @@ void curses_print() {
         fprintf(stderr, "Error initialising ncurses.\n");
         exit(EXIT_FAILURE);
     }
-
 
     /*  Display "Hello, world!" in the centre of the
         screen, call refresh() to show our changes, and
@@ -136,26 +40,10 @@ void curses_print() {
 int main(int argc, char* argv[])
 {
 #ifdef CURSES
-    curses_print();
+  //  curses_print();
 #endif
-  try
-  {
-    if (argc != 2)
-    {
-      std::cerr << "Usage: async_tcp_echo_server <port>\n";
-      return 1;
-    }
-
-    boost::asio::io_service io_service;
-
-    server s(io_service, std::atoi(argv[1]));
-
-    io_service.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
+  auto game1 = std::unique_ptr<Game>(new Game());
+  auto game2 = std::unique_ptr<Game>(new Game());
 
   return 0;
 }
